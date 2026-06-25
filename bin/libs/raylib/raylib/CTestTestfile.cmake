ec087b4bf8704b432b18a1df7c705394b6c244ab
BlopRoy <210707460+BlopRoy@users.noreply.github.com>
2026-06-25 18:58:25 +0200

    netoyage

--- bin/libs/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/usr/Documents/GitHub/ReactronProjetECEPREPAC2026/libs/raylib/src
-# Build directory: C:/Users/usr/Documents/GitHub/ReactronProjetECEPREPAC2026/bin/libs/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
